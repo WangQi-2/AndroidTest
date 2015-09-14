d5bb14e831a7bab0038f49a2def1a20890682d4c
王其 <wangqi504635@gmail.com>
2015-09-14 23:36:14 +0800

    add numbertextview

---
+++ app/src/main/java/com/wq/androidtest/view/customview/NumberTextView.java
@@ -0,0 +1,23 @@
+package com.wq.androidtest.view.customview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+/**
+ * Created by wangqi on 15/9/14.
+ */
+public class NumberTextView extends TextView{
+    public NumberTextView(Context context) {
+        super(context);
+    }
+
+    public NumberTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    //TODO how to use
+    public NumberTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
