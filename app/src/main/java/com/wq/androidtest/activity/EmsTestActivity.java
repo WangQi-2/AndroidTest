6377812e12f6e0445642b167ebdabdd159e8906d
王其 <wangqi504635@gmail.com>
2015-09-14 11:05:53 +0800

    test ems

---
+++ app/src/main/java/com/wq/androidtest/activity/EmsTestActivity.java
@@ -0,0 +1,14 @@
+package com.wq.androidtest.activity;
+
+import android.os.Bundle;
+
+/**
+ * Created by wangqi on 15/9/14.
+ */
+public class EmsTestActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
