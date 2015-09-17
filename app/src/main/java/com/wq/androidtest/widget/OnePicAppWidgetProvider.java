d767e72cb3e7f539002767dafa805556c93a1981
王其 <wangqi504635@gmail.com>
2015-09-17 13:47:33 +0800

    add app widget

---
+++ app/src/main/java/com/wq/androidtest/widget/OnePicAppWidgetProvider.java
@@ -0,0 +1,9 @@
+package com.wq.androidtest.widget;
+
+import android.appwidget.AppWidgetProvider;
+
+/**
+ * Created by wangqi on 15/9/17.
+ */
+public class OnePicAppWidgetProvider extends AppWidgetProvider{
+}
