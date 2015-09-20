448e39f724f2358b16e9db84a0aae37eb8031da8
王其 <wangqi504635@gmail.com>
2015-09-20 10:41:55 +0800

    add application and crash handler

---
+++ app/src/main/java/com/wq/androidtest/base/CrashHandler.java
@@ -0,0 +1,121 @@
+package com.wq.androidtest.base;
+
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Environment;
+import android.os.Process;
+
+import com.wq.androidtest.util.Logger;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by wangqi on 15/9/20.
+ */
+public class CrashHandler implements Thread.UncaughtExceptionHandler {
+
+    private static final String TAG = CrashHandler.class.getSimpleName();
+    private static CrashHandler sInstance = new CrashHandler();
+    private Thread.UncaughtExceptionHandler mDefaultCrashHandler;
+    private Context mContext;
+
+    private CrashHandler() {
+
+    }
+
+    public static CrashHandler getInstance() {
+        return sInstance;
+    }
+
+    public void init(Context context) {
+        // TODO: 15/9/20
+        mDefaultCrashHandler = Thread.getDefaultUncaughtExceptionHandler();
+        // TODO: 15/9/20
+        Thread.setDefaultUncaughtExceptionHandler(this);
+        // TODO: 15/9/20
+        mContext = context.getApplicationContext();
+    }
+
+    // TODO: 15/9/20
+    @Override
+    public void uncaughtException(Thread thread, Throwable ex) {
+        try {
+            dumpExceptionToSDCard(ex);
+            uploadExceptionToServer(ex);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        ex.printStackTrace();
+        if (mDefaultCrashHandler != null) {
+            mDefaultCrashHandler.uncaughtException(thread, ex);
+        } else {
+            // TODO: 15/9/20 java.lang.Process vs android.os.Process
+            android.os.Process.killProcess(Process.myPid());
+        }
+    }
+
+
+    // TODO: 15/9/20  
+    private void uploadExceptionToServer(Throwable ex) {
+
+    }
+
+    private static final String PATH = Environment.getExternalStorageDirectory().getPath() + "/AndroidTest/log/";
+    private static final String FILE_NAME = "crash";
+    private static final String FILE_NAME_SUFFIX = ".trace";
+
+    // TODO: 15/9/20 need?? yes ,add quanju debug
+    boolean DEBUG = false;
+
+    private void dumpExceptionToSDCard(Throwable ex) {
+        if (!Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+            // TODO: 15/9/20  add logger.w
+            Logger.e(TAG, "sdcard unmounted,skip dump exception");
+            return;
+        }
+
+        File dir = new File(PATH);
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+
+        long current = System.currentTimeMillis();
+        // TODO: 15/9/20 add a util
+        String time = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date(current));
+        File file = new File(PATH + FILE_NAME + time + FILE_NAME_SUFFIX);
+
+        try {
+            //// TODO: 15/9/20  
+            PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(file)));
+            pw.println(time);
+            PackageManager pm = mContext.getPackageManager();
+            PackageInfo pi = pm.getPackageInfo(mContext.getPackageName(), PackageManager.GET_ACTIVITIES);
+            //app
+            pw.print("App version: ");
+            pw.print(pi.versionName);
+            pw.print("-");
+            pw.println(pi.versionCode);
+            //system
+            pw.print("OS Verion: ");
+            pw.print(Build.VERSION.RELEASE);
+            pw.print("-");
+            pw.println(Build.VERSION.SDK_INT);
+            //device
+            pw.print("Vender: ");
+            pw.println(Build.MANUFACTURER);
+            pw.print("Model: ");
+            pw.println(Build.MODEL);
+            pw.print("CPU ABI: ");
+            pw.println(Build.CPU_ABI);
+        } catch (Exception e) {
+            Logger.e(TAG, "failed to dump crash " + e.getMessage());
+        }
+    }
+}
---
+++ app/src/main/java/com/wq/androidtest/base/TestApplication.java
@@ -0,0 +1,25 @@
+package com.wq.androidtest.base;
+
+import android.app.Application;
+
+/**
+ * Created by wangqi on 15/9/20.
+ */
+public class TestApplication extends Application {
+
+    private static TestApplication sInstance;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        sInstance = this;
+
+
+        //crash
+        CrashHandler.getInstance().init(this);
+    }
+
+    public static TestApplication getsInstance() {
+        return sInstance;
+    }
+}
