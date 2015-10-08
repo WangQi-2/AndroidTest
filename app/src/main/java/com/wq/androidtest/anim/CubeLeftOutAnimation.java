60f760de83d008ec428b7e19cdb1e84f8651d65a
王其 <wangqi504635@gmail.com>
2015-10-08 12:28:51 +0800

    camera

--- app/src/main/java/com/wq/androidtest/anim/CubeLeftOutAnimation.java
+++ app/src/main/java/com/wq/androidtest/anim/CubeLeftOutAnimation.java
@@ -29,13 +29,25 @@ public class CubeLeftOutAnimation extends Animation {
         super.applyTransformation(interpolatedTime, t);
         float rotate = (-finalDegree * interpolatedTime);
         camera.save();
-        camera.translate(width - interpolatedTime * width, 0,0);
+        camera.translate(width - interpolatedTime * width, 0, 0);
+        // TODO: 15/10/8 环绕Y轴旋转
         camera.rotateY(rotate);
         camera.getMatrix(matrix);
         camera.restore();
 
-        matrix.postTranslate(0,height/2);
-        matrix.preTranslate(-width, -height/2);
+//        public static final int MSCALE_X = 0;   //!< use with getValues/setValues
+//        public static final int MSKEW_X  = 1;   //!< use with getValues/setValues
+//        public static final int MTRANS_X = 2;   //!< use with getValues/setValues
+//        public static final int MSKEW_Y  = 3;   //!< use with getValues/setValues
+//        public static final int MSCALE_Y = 4;   //!< use with getValues/setValues
+//        public static final int MTRANS_Y = 5;   //!< use with getValues/setValues
+//        public static final int MPERSP_0 = 6;   //!< use with getValues/setValues
+//        public static final int MPERSP_1 = 7;   //!< use with getValues/setValues
+//        public static final int MPERSP_2 = 8;   //!< use with getValues/setValues
+        // TODO: 15/10/8 后乘是行转换
+        matrix.postTranslate(0, height / 2);
+        // TODO: 15/10/8 前乘是列转换
+        matrix.preTranslate(-width, -height / 2);
 
         t.getMatrix().postConcat(matrix);
     }
