17ce007040fa95493ca4ab41b819d2f5a6ed46e8
王其 <wangqi504635@gmail.com>
2015-11-06 15:29:18 +0800

    add horizontalgridview

---
+++ app/src/main/java/com/wq/androidtest/activity/ScrollViewActivity.java
@@ -0,0 +1,7 @@
+package com.wq.androidtest.activity;
+
+/**
+ * Created by wangqi on 15/11/6.
+ */
+public class ScrollViewActivity {
+}
---
+++ app/src/main/java/com/wq/androidtest/activity/system/HorizontalGridViewActivity.java
@@ -0,0 +1,7 @@
+package com.wq.androidtest.activity.system;
+
+/**
+ * Created by wangqi on 15/11/4.
+ */
+public class HorizontalGridViewActivity {
+}
